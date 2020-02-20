f449afacd22264066fb6d36c90bb3d7f25f0dfdc
Windzard <lynnliu.windzard@gmail.com>
2020-02-20 18:03:22 +0800

    Reforge & Simplify

--- Essistant.py
+++ Essistant.py
@@ -1,16 +1,14 @@
 '''
-Welcome to Essistant v1.4.7, sync test
+Welcome to Essistant v1.5, Reforge & Simplify
 '''
 
 # pyinstaller -i "C:\Users\LynnL\Pictures\Saved Pictures\sadpanda.ico" -p "C:\Program Files (x86)\Microsoft Visual studio\Shared\Python37_64" -p "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Lib" -p "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python37_64\Lib\site-packages" -F Essistant.py
 
 from requests import get
 from bs4 import BeautifulSoup
-import xlwt
-import xlrd
-from xlutils.copy import copy
-from datetime import datetime
+import pandas as pd
 import progressbar as pb
+# import sys
 
 headers = {
     'User-Agent':
@@ -24,40 +22,47 @@ proxies = {
 }
 
 
+def SEARCH(artist):
+    url = 'https://e-hentai.org/lofi/?f_search=chinese+artist%3A'\
+        + artist + '&f_apply=Search'
+    try:
+        response = get(url, proxies=proxies, headers=headers, timeout=5)
+    except ConnectionError:
+        # print(sys.exc_info()[0])
+        print('Failed to refresh, please try to change your IP adress.\n')
+        return
+    soup = BeautifulSoup(response.text, 'html.parser')
+    try:
+        work = soup.select('a.b')
+        time = soup.select('td.ip')
+        return work[0].contents[0], time[1].contents[0]
+    except AttributeError:
+        print('Seems like there are no results found.\n')
+        return
+
+
 def ADD():
     artist = input('Artist: ')
     try:
-        old = xlrd.open_workbook('log.xls')
-        oldsheet = old.sheets()[0]
-        num = oldsheet.nrows
-        new = copy(old)
+        log = pd.read_csv('log.csv', encoding='utf-8')
     except FileNotFoundError as e:
         # print(sys.exc_info()[0])
         print('WARNING: ' + str(e))
-        print('Creating new log file.\n')
-        old = xlwt.Workbook(encoding='utf-8')
-        oldsheet = old.add_sheet('log')
-        num = 0
-        new = old
-    for x in range(num):
-        if artist == oldsheet.cell(x, 0).value:
+        print('Creating new log file...\n')
+        log = pd.DataFrame(columns=['Artists', 'Works', 'Time'])
+    for x in range(len(log)):
+        if artist == log.iloc[x, 0]:
             print('You have already added this artist.\n')
             return
-    url = 'https://e-hentai.org/lofi/?f_search=chinese+artist%3A'\
-        + artist + '&f_apply=Search'
-    response = get(url, proxies=proxies, headers=headers)
-    # response = get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    try:
-        work = soup.tr.tr.td.a.contents
-    except AttributeError as e:
-        print('WARNING: ' + str(e))
-        print('Sorry, no such artist.\n')
-        return
-    newsheet = new.get_sheet(0)
-    newsheet.write(num, 0, artist)
-    newsheet.write(num, 1, work)
-    new.save('log.xls')
+    latest_work, post_time = SEARCH(artist)
+    log = log.append(
+        {
+            'Artists': artist,
+            'Works': latest_work,
+            'Time': post_time
+        },
+        ignore_index=True)
+    log.to_csv('log.csv', index=False, encoding='utf-8')
     print('Successfully added!\n')
 
 
@@ -65,12 +70,11 @@ def REFRESH():
     print('Refreshing...')
     news = []
     try:
-        workbook = xlrd.open_workbook('log.xls')
+        log = pd.read_csv('log.csv', encoding='utf-8')
     except FileNotFoundError:
-        print('The list is empty, please add someone before you refresh.\n')
+        print('The list is empty.\n')
         return
-    worksheet = workbook.sheets()[0]
-    num = worksheet.nrows
+
     widgets = [
         pb.AnimatedMarker(markers='|\\-/'), ' ', 'Progress: ',
         pb.Bar(marker='=', left='[', right=']'), ' ',
@@ -78,50 +82,32 @@ def REFRESH():
         pb.Percentage(), ' ',
         pb.AdaptiveETA()
     ]
-    pbar = pb.ProgressBar(widgets=widgets, maxval=num).start()
-    begin = datetime.now()
-    for x in range(0, num):
-        artist = worksheet.cell(x, 0).value
-        url = 'https://e-hentai.org/lofi/?f_search=chinese+artist%3A'\
-            + artist + '&f_apply=Search'
-        response = get(url, proxies=proxies, headers=headers)
-        # response = get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        try:
-            work = soup.tr.tr.td.a.contents[0]
-            if work != worksheet.cell(x, 1).value:
-                old = xlrd.open_workbook('log.xls')
-                oldsheet = old.sheets()[0]
-                num = oldsheet.nrows
-                new = copy(old)
-                newsheet = new.get_sheet(0)
-                news.append(worksheet.cell(x, 0).value)
-                newsheet.write(x, 1, soup.tr.tr.td.a.contents)
-        except AttributeError:
-            print('Failed to refresh, please try to change your IP adress.\n')
-            return
+    pbar = pb.ProgressBar(widgets=widgets, maxval=len(log)).start()
+
+    for x in range(0, len(log)):
+        latest_work, post_time = SEARCH(log.iloc[x, 0])
+        if post_time != log.iloc[x, 2]:
+            log.iloc[x, 1] = latest_work
+            log.iloc[x, 2] = post_time
+            news.append(log.iloc[x, 0] + ' - ' + post_time)
         pbar.update(x + 1)
-    new.save('log.xls')
-    end = datetime.now()
+    log.to_csv('log.csv', index=False, encoding='utf-8')
     print('\n%-2d new work(s) found!\n' % len(news) + '-' * 20)
     for x in news:
         print(x)
     print('-' * 20)
-    time = (end - begin).total_seconds()
-    print('Running time: ' + str(time) + 's\nAverage time: %fs\n' %
-          (time / num))
     return
 
 
 def main():
     REFRESH()
     while True:
-        retort = input(
-            'a. add\nb. refresh\nClick enter to exit\nChoose your action:\n')
+        opt = input(
+            'a. add\nb. refresh\nClick Enter to exit\nChoose your action:\n')
         print()
-        if retort == 'a':
+        if opt == 'a':
             ADD()
-        elif retort == 'b':
+        elif opt == 'b':
             REFRESH()
         else:
             exit()
